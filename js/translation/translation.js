32bb6332ee0054594f8acd21b37485c8e1a32da3
Marouen <marouen.abdi@hotmail.com>
2024-09-05 14:33:44 +0100

    Fix translation

---
+++ js/translation/translation.js
@@ -0,0 +1,84 @@
+// Function to change language and update UI
+function changeLanguage(lang) {
+	fetch(`/js/translation/${lang}.json`)
+		.then((response) => response.json())
+		.then((data) => {
+			document.querySelectorAll('[data-translate]').forEach((el) => {
+				const key = el.getAttribute('data-translate');
+				if (data[key]) {
+					el.innerHTML = data[key];
+				}
+			});
+
+			document
+				.querySelectorAll('[data-translate-placeholder]')
+				.forEach((el) => {
+					const key = el.getAttribute('data-translate-placeholder');
+					if (data[key]) {
+						el.setAttribute('placeholder', data[key]);
+					}
+				});
+
+			document.querySelectorAll('[data-translate-button]').forEach((el) => {
+				const key = el.getAttribute('data-translate-button');
+				if (data[key]) {
+					el.innerHTML = data[key];
+				}
+			});
+		})
+		.catch((error) => {
+			console.error('Error loading translation file:', error);
+		});
+}
+
+// Function to set and save the selected language
+function setLanguage(lang) {
+	localStorage.setItem('selectedLang', lang); // Save selected language in localStorage
+	const langButtons = {
+		en: document.getElementById('lang-en'),
+		fr: document.getElementById('lang-fr'),
+		pt: document.getElementById('lang-pt'),
+	};
+
+	for (let key in langButtons) {
+		if (key === lang) {
+			langButtons[key].classList.add('disabled');
+			langButtons[key].setAttribute('disabled', 'disabled');
+		} else {
+			langButtons[key].classList.remove('disabled');
+			langButtons[key].removeAttribute('disabled');
+		}
+	}
+	changeLanguage(lang); // Update the language content on the page
+}
+
+// Initialize language on page load
+document.addEventListener('DOMContentLoaded', () => {
+	const defaultLang = 'en';
+	const systemLang = (navigator.language || navigator.userLanguage).substring(
+		0,
+		2
+	);
+
+	// Check if the user has selected a language previously; if not, fall back to system language
+	const savedLang = localStorage.getItem('selectedLang') || systemLang;
+
+	const lang = savedLang.startsWith('fr')
+		? 'fr'
+		: savedLang.startsWith('pt')
+		? 'pt'
+		: defaultLang;
+
+	setLanguage(lang);
+
+	// Add event listeners for language buttons
+	document
+		.getElementById('lang-en')
+		.addEventListener('click', () => setLanguage('en'));
+	document
+		.getElementById('lang-fr')
+		.addEventListener('click', () => setLanguage('fr'));
+	document
+		.getElementById('lang-pt')
+		.addEventListener('click', () => setLanguage('pt'));
+});
