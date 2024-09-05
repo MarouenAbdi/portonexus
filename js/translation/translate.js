792fbe85bb33dc5f4090e350117e25c96e2aaa3b
Marouen <marouen.abdi@hotmail.com>
2024-09-05 10:56:43 +0100

    update translation module

--- js/translation/translate.js
+++ js/translation/translate.js
@@ -1,22 +1,23 @@
 function changeLanguage(lang) {
-	// Load the language file
 	fetch(`/js/translation/${lang}.json`)
 		.then((response) => response.json())
-		.then((translationData) => {
-			// Get all elements with the data-translate attribute
-			document.querySelectorAll('[data-translate]').forEach((element) => {
-				// Get the key for the translation
-				const key = element.getAttribute('data-translate');
-
-				// Look for the translation in the loaded JSON data
-				if (translationData[key]) {
-					element.textContent = translationData[key];
-				} else {
-					console.warn(`Translation key '${key}' not found in '${lang}.json'`);
+		.then((data) => {
+			// Update text content
+			document.querySelectorAll('[data-translate]').forEach((el) => {
+				const key = el.getAttribute('data-translate');
+				if (data[key]) {
+					el.innerHTML = data[key];
 				}
 			});
-		})
-		.catch((error) => {
-			console.error(`Error loading ${lang}.json:`, error);
+
+			// Update placeholders
+			document
+				.querySelectorAll('[data-translate-placeholder]')
+				.forEach((el) => {
+					const placeholderKey = el.getAttribute('data-translate-placeholder');
+					if (data[placeholderKey]) {
+						el.setAttribute('placeholder', data[placeholderKey]);
+					}
+				});
 		});
 }
