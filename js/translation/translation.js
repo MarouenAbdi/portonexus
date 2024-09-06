461f9ffb468246c664869080942600418d16dfde
Marouen <marouen.abdi@hotmail.com>
2024-09-06 13:46:00 +0100

    Improve performance

--- js/translation/translation.js
+++ js/translation/translation.js
@@ -15,9 +15,9 @@ function changeLanguage(lang) {
 			document
 				.querySelectorAll('[data-translate-placeholder]')
 				.forEach((el) => {
-					const key = el.getAttribute('data-translate-placeholder');
-					if (data[key]) {
-						el.setAttribute('placeholder', data[key]);
+					const placeholderKey = el.getAttribute('data-translate-placeholder');
+					if (data[placeholderKey]) {
+						el.setAttribute('placeholder', data[placeholderKey]);
 					}
 				});
 
